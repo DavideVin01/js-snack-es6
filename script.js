19ece8c74a020ea3e06b4b107871202b88e24460
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-01-20 17:44:50 +0100

    Completes 2nd snack (1st still incomplete)

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('JS OK');
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+const bikes = [
+    {
+        nome: 'Mountain Bike',
+        peso: 22,
+    },
+    {
+        nome: 'Bianchi',
+        peso: 27,
+    },
+    {
+        nome: 'Olmo',
+        peso: 30,
+    },
+]
+
+//
---
+++ script2.js
@@ -0,0 +1,28 @@
+// Snack2
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadre = [
+    { nome: 'Milan', punti: 0, falli: 0, },
+    { nome: 'Juventus', punti: 0, falli: 0, },
+    { nome: 'Inter', punti: 0, falli: 0, },
+]
+
+const getRandomNumber = (min, max) => {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNumber;
+}
+
+for (let i = 0; i < squadre.length; i++) {
+    for (let key in squadre) {
+        const currentTeam = squadre[i];
+
+        currentTeam.punti = getRandomNumber(1, 20);
+        currentTeam.falli = getRandomNumber(1, 10);
+    }
+}
+
+console.table(squadre);
